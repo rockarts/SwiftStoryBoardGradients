e73ad5274a69d7c666d545d55ee7f0056b2abdb6
Steven Rockarts <stevenrockarts@gmail.com>
2017-12-27 14:03:07 -0700

    Create GradientView.swift

---
+++ GradientView.swift
@@ -0,0 +1,37 @@
+//
+//  GradientView.swift
+//
+//  Created by Steven Rockarts on 2017-12-27.
+//  Copyright © 2017 Steven Rockarts. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var FirstColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var SecondColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        get{
+            return CAGradientLayer.self
+        }
+    }
+    
+    func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [FirstColor.cgColor, SecondColor.cgColor]
+        //Change in the middle
+        layer.locations = [0.5]
+    }
+}
